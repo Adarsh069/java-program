e187e437c5d671c8797de7ba8b8986f5d44fd442
Adarsh069 <115801224+Adarsh069@users.noreply.github.com>
2024-11-10 18:13:18 +0530

    Add files via upload

---
+++ Day1/std.java
@@ -0,0 +1,15 @@
+class std {
+    public static void main(String args[]) {
+        String Std_name = "Adarsh";
+        int Std_id = 1;
+        char Std_gend = 'M';
+        int Std_marks = 75;
+        String Std_subject = "JAVA";
+
+        System.out.println("Given Student Name: " + Std_name);
+        System.out.println("Given Student ID: " + Std_id);
+        System.out.println("Given Student Gender: " + Std_gend);
+        System.out.println("Given Student Marks: " + Std_marks);
+        System.out.println("Given Student Subject: " + Std_subject);
+    }
+}
